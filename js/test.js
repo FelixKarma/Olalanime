c7b6b2281f6114f17d3d2f37254b5cfb7cb12cce
Soraa591 <soraaa591@.gmail.com>
2023-12-11 10:55:25 +0100

    pull journalier

--- js/test.js
+++
@@ -1,9 +0,0 @@
-function isPlaindrome (word) {
-    const reversedWord = word
-    .split ('')
-    .reverse ('')
-    .join ('')
-    return word.toUpperCase() === reversedWord.toUpperCase()
-    
-}
-console.log(isPlaindrome('kayak'))
