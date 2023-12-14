f6e048ec43f02540249f0de20b081f0533b6155b
Joohde <horscadre01@gmail.com>
2023-12-14 14:05:35 +0100

    correction de bugs

--- js/connexion.js
+++ js/connexion.js
@@ -4,49 +4,48 @@ document.addEventListener('DOMContentLoaded', function () {
         const emailInput = document.getElementsByName('email')[0];
         const passwordInput = document.getElementsByName('password')[0];
         const statusElement = document.getElementById('status1');
-        
+        let isLoggedIn = false;
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(emailInput.value)) {
             statusElement.textContent = 'Veuillez saisir une adresse e-mail valide.';
-            return;
+            return isLoggedIn;
         }
-
 
         const userDataString = localStorage.getItem(emailInput.value);
 
         if (!userDataString) {
             statusElement.textContent = 'Aucun compte trouvé avec cet email.';
-            return;
+            return isLoggedIn;
         }
 
         const userData = JSON.parse(userDataString);
 
-
         if (passwordInput.value !== userData.password) {
             statusElement.textContent = 'Mot de passe incorrect.';
-            return;
+            return isLoggedIn;
         }
-        
+
+        isLoggedIn = true;
         localStorage.setItem('isLoggedIn', 'true');
-        
+        return isLoggedIn;
     }
-    
-    
+
     const loginButton = document.getElementById('btnSeConnecter');
-    
+
     loginButton.addEventListener('click', function (event) {
         event.preventDefault();
-        loginUser();
-        const previousPage = localStorage.getItem('previousPage');
-        if (previousPage) {
-            document.getElementById("formSeConnecter").setAttribute("action",previousPage)
-            window.location.href = previousPage;
-        } else {
-        window.location.href = '../html/Accueil.html';
+        const isLoginSuccessful = loginUser();
+
+        if (isLoginSuccessful) {
+            const previousPage = localStorage.getItem('previousPage');
+            if (previousPage) {
+                document.getElementById("formSeConnecter").setAttribute("action", previousPage);
+                window.location.href = previousPage;
+            } else {
+                window.location.href = '../html/Accueil.html';
+            }
         }
     });
 });
-
-
 
