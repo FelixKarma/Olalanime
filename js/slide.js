7351893a8a4e7282534e250c410790ffae16e9fa
Soraa591 <soraaa591@.gmail.com>
2023-12-14 21:16:03 +0100

    ajout sur javascript slide

--- js/slide.js
+++ js/slide.js
@@ -1,7 +1,25 @@
 function scrollWin() {
-    window.scrollBy(0 , 607)
+    let resultat = vhToPixels(100)
+    window.scrollBy(0 , resultat)
 }
 
 function scrollWin2() {
-    window.scrollBy(0 , -607 ) 
+    let resultat = vhToPixels(100)
+    window.scrollBy(0 , -resultat ) 
 }
+
+function vhToPixels(vh) {
+    // Obtenir la hauteur de la fenêtre en pixels
+    const windowHeight = window.innerHeight;
+  
+    // Calculer la valeur en pixels
+    const pixels = (vh * windowHeight) / 100;
+  
+    // Retourner le résultat arrondi
+    return Math.round(pixels);
+  }
+  
+  // Exemple d'utilisation :
+  const vhValue = 50; // Remplacez cela par la valeur en vh que vous souhaitez convertir
+  const pixelsValue = vhToPixels(vhValue);
+  console.log(`${vhValue}vh équivaut à ${pixelsValue}px`);
